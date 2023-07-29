468091d399dc5acf5028419410ea0d753abe16c0
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-07-29 07:08:21 +0000

    ✨ Add an example action

--- app/lint-staged.config.cjs
+++ app/lint-staged.config.cjs
@@ -1,6 +1,10 @@
 /** @type { import("lint-staged").Config } */
 const config = {
-  "*.{tsx,ts,js}": ["bash -c 'yarn check:type'", "prettier --write", "eslint --cache --fix"],
+  "*.{tsx,ts,js}": [
+    "bash -c 'yarn check:type'",
+    "prettier --write",
+    "eslint --cache --fix",
+  ],
   "*.json": ["prettier --write"],
   "*.md": ["prettier --write", "textlint"],
 };
--- app/modules/example/index.ts
+++ app/modules/example/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import core from "@actions/core";
+
+const run = (): void => {
+  try {
+    core.info("Hello World");
+  } catch (err) {
+    if (err instanceof Error) core.setFailed(err.message);
+  }
+};
+
+run();
