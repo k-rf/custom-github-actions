f6d433cfa670e15daeaf27bbc6fea4d86019cdea
Kawano Kenichi <kenichi.k.820@gmail.com>
2023-07-29 06:39:27 +0000

    ✨ Add app container configurations

---
+++ app/lint-staged.config.cjs
@@ -0,0 +1,8 @@
+/** @type { import("lint-staged").Config } */
+const config = {
+  "*.{tsx,ts,js}": ["bash -c 'yarn check:type'", "prettier --write", "eslint --cache --fix"],
+  "*.json": ["prettier --write"],
+  "*.md": ["prettier --write", "textlint"],
+};
+
+module.exports = config;
